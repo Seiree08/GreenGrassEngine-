5c8fc9e9d823f6ef92d334621d24208c2baae4ad
Seiree08 <rmoreno31367@ucq.edu.mx>
2024-05-27 23:45:26 -0600

    Window Class Implementation
    Creamos el "BaseApp" y el "window" class

---
+++ GreenGrassEngine/Include/PreRequisites.h
@@ -0,0 +1,13 @@
+#pragma once
+//Librerías STD
+#include <string>
+#include <windows.h>
+#include <xnamath.h>
+
+//Librerías DirectX
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include "resource.h"
+
+//Third Parties
